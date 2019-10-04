1547c7c154d21d2201f46b90a86bf6c28df8df4e
Salvador Zaragoza <salvador.zaragoza@uabc.edu.mx>
2019-10-04 07:41:04 -0700

    -Added a button that tries to change color but it fails at it

--- AwesomeProjectExpo/App.js
+++ AwesomeProjectExpo/App.js
@@ -1,39 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
 
-export default class myButton extends React.Component() {
-  constructor(props) {
-    super(props);
-    this.state = {
-      buttonColor: 'red',
-    };
-  }
+export default class myButton extends Component() {
 
-  onPressButton = () => {
-    if (this.state.buttonColor === 'red'){
-      this.setState({ buttonColor: 'green' });
-    }
-    else {
-      this.setState({ buttonColor: 'red' });
-    }
-  }
+  // constructor(){
+  //   super()
+  //   this.state = {
+  //     buttonColor: 'red',
+  //   }
+  // }
+
+  // onPressButton = () => {
+  //   if (this.state.buttonColor === 'red'){
+  //     this.setState({ buttonColor: 'green' });
+  //   }
+  //   else {
+  //     this.setState({ buttonColor: 'red' });
+  //   }
+  // }
 
   render () {
     return (
-      <View style = {styles.viewMain}>
-        <Button
-          color = {this.state.buttonColor},
-          title = "Press me",
-          onPress = {this.onPressButton},
-        />
+      <View style = {{flex: 1, justifyContent: 'center'}}>
+        <Text>Hello world</Text>
+
+        {/* <Button
+          color = {this.state.buttonColor}
+          title = "Press me"
+          onPress = {this.onPressButton}
+        /> */}
       </View>
     )
   }
-
-  const styles = StyleSheet.create({
-    viewMain: {
-      flex: 1,
-      justifyContent: 'center',
-    }
-  });
 }
